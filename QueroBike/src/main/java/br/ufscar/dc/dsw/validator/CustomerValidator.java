38fe3fa6ba5363aa62c2a2a8cabd2cb81c53df9d
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-11-29 01:23:26 -0300

    Add method to validate login inputs

--- QueroBike/src/main/java/br/ufscar/dc/dsw/validator/CustomerValidator.java
+++ QueroBike/src/main/java/br/ufscar/dc/dsw/validator/CustomerValidator.java
@@ -24,15 +24,15 @@ public class CustomerValidator {
         String gender = request.getParameter("gender");
         String birthdateString = request.getParameter("birthdate");
 
-        if (email == null || email.length()==0) {
+        if (email == null || email.length() == 0) {
             errors.add("É obrigatório informar o e-mail");
         }
 
-        if (password == null || password.length()==0) {
+        if (password == null || password.length() == 0) {
             errors.add("É obrigatório informar a senha");
         }
 
-        if (cpf == null || cpf.length()==0) {
+        if (cpf == null || cpf.length() == 0) {
             errors.add("É obrigatório informar o CPF");
         } else {
             if (cpf.length() != 11) {
@@ -44,7 +44,7 @@ public class CustomerValidator {
             errors.add("É obrigatório informar o nome");
         }
 
-        if (phone == null|| phone.length()==0) {
+        if (phone == null || phone.length() == 0) {
             errors.add("É obrigatório informar o telefone");
         } else {
             final String regex = "^55\\d{10,11}$";
@@ -59,11 +59,11 @@ public class CustomerValidator {
             }
         }
 
-        if (gender == null || gender.length()==0) {
+        if (gender == null || gender.length() == 0) {
             errors.add("É obrigatório informar o gênero");
         }
 
-        if (birthdateString == null || birthdateString.length()==0) {
+        if (birthdateString == null || birthdateString.length() == 0) {
             errors.add("É obrigatório informar a data de nascimento");
         } else {
             try {
@@ -77,6 +77,23 @@ public class CustomerValidator {
                         "O formato da data de nascimento é inválido. Deve ser dia/mês/ano"
                 );
             }
+        }
+
+        return errors;
+    }
+
+    public static List<String> loginCustomerValidation(HttpServletRequest request) {
+        List<String> errors = new ArrayList();
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        if (email == null || email.length() == 0) {
+            errors.add("É obrigatório informar o e-mail");
+        }
+
+        if (password == null || password.length() == 0) {
+            errors.add("É obrigatório informar a senha");
         }
 
         return errors;
