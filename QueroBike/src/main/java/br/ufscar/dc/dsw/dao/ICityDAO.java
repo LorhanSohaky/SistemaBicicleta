62adfed6964607f45c21b4ee6f0e1596adf7cf7a
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-12-29 03:14:13 -0300

    Creates city classes

---
+++ QueroBike/src/main/java/br/ufscar/dc/dsw/dao/ICityDAO.java
@@ -0,0 +1,19 @@
+package br.ufscar.dc.dsw.dao;
+
+import br.ufscar.dc.dsw.domain.City;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface ICityDAO extends CrudRepository<City, City> {
+
+    City findById(int id);
+
+    List<City> findAll();
+
+    City save(City city);
+
+    @Query("SELECT c FROM City c WHERE c.name = :name AND c.state = :state")
+    public List<City> find(@Param("name") String name, @Param("state") String state);
+}
