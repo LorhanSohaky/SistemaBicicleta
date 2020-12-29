06c6ed73d80467b6f841deb06cdfaec19bdb09b0
Lorhan Sohaky <lorhansohaky@gmail.com>
2020-12-29 03:13:47 -0300

    Refactore imports

--- QueroBike/src/main/java/br/ufscar/dc/dsw/config/MvcConfig.java
+++ QueroBike/src/main/java/br/ufscar/dc/dsw/config/MvcConfig.java
@@ -3,14 +3,9 @@ package br.ufscar.dc.dsw.config;
 
 import java.util.Locale;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
