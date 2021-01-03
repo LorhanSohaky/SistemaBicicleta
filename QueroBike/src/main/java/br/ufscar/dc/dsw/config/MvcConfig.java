bbc925118f8e4b82701860963330a2331fc52fb1
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-01-02 23:49:03 -0300

    Add login page

--- QueroBike/src/main/java/br/ufscar/dc/dsw/config/MvcConfig.java
+++ QueroBike/src/main/java/br/ufscar/dc/dsw/config/MvcConfig.java
@@ -14,8 +14,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("redirect:/home");
-        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
     }
 
