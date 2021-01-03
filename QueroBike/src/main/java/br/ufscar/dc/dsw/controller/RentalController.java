3d2fe8b92ee9ba7a3555dd42e625cbe740dd451b
Lorhan Sohaky <lorhansohaky@gmail.com>
2021-01-02 23:00:47 -0300

    Add rentals list

---
+++ QueroBike/src/main/java/br/ufscar/dc/dsw/controller/RentalController.java
@@ -0,0 +1,26 @@
+package br.ufscar.dc.dsw.controller;
+
+import br.ufscar.dc.dsw.domain.Rental;
+import br.ufscar.dc.dsw.service.spec.IRentalService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rentals")
+public class RentalController {
+
+    @Autowired
+    private IRentalService rentalService;
+
+    @GetMapping("/")
+    public String listRentals(ModelMap model) {
+        List<Rental> rentals = rentalService.listAll();
+        model.addAttribute("rentals", rentals);
+
+        return "rental/list";
+    }
+}
